7bf0f784c11971aa09dbfe796ce78155a4148903
wilkdasilk <anwilk92@gmail.com>
2017-03-10 15:16:42 -0800

    user done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,24 @@ class UsersController < ApplicationController\
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
-  root to: "users#index"
+  root to: 'users#index'
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create', as: 'create_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/login', to: 'sessions#new'
 
 end
